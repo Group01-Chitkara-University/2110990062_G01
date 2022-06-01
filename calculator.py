2737ca65d939f247419e825e808c3921959f6c80
Aayushkatoch <aayush0028.be21@chitkara.edu.in>
2022-06-01 16:12:48 +0530

    subtration

--- calculator.py
+++ calculator.py
@@ -3,6 +3,9 @@
 # This function adds two numbers
 def add(x, y):
     return x + y
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
 # This function multiplies two numbers
 def multiply(x, y):
     return x * y
@@ -24,6 +27,8 @@ while True:
 
     if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
+elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
 elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2)
 
